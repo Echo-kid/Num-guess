7420bffe14469335d8d1c28e9e233b1222c31399
Lockett Pundt <kalifornium@hotmail.com>
2019-12-16 12:29:37 -0500

    changed the 'for' loop to count down. also added an 'else if' to let the user know how many guess are left

--- Guess-Number.js
+++ Guess-Number.js
@@ -1,11 +1,15 @@
+  
 let randoNum = Math.floor((Math.random() * 100) + 1);
 let guess = prompt("Guess a number between 1 and 100. You have 10 guesses...");
 // for loop to give 10 guesses.
 function guessNumber(randoNum) {
-    for (var i = 0; i < 10; i++) {
+    for (var i = 10; i > 0; i--) {
         if (guess === randoNum) {
             alert("Congrats! You chose correctly!")
             break;
+        }
+        else if (guess !== randoNum) {
+            alert("You now have " + i + " guesses left...");
         }
     }
 }
