9e24ae4f890a62b922447a9c66329252020e43a6
Lockett Pundt <kalifornium@hotmail.com>
2019-12-16 12:39:06 -0500

    added an else statement but i'm not sure if it works in the end. also added a new prompt so that the user can keep guessing. it was set to only one guess initially.

--- Guess-Number.js
+++ Guess-Number.js
@@ -1,16 +1,21 @@
-  
+
 let randoNum = Math.floor((Math.random() * 100) + 1);
 let guess = prompt("Guess a number between 1 and 100. You have 10 guesses...");
 // for loop to give 10 guesses.
 function guessNumber(randoNum) {
-    for (var i = 10; i > 0; i--) {
+    for (var i = 9; i > 0; i--) {
         if (guess === randoNum) {
             alert("Congrats! You chose correctly!")
             break;
         }
         else if (guess !== randoNum) {
             alert("You now have " + i + " guesses left...");
+            guess = prompt("Guess again...");
+        }
+        else {
+        	alert("You are out of guesses");
         }
     }
 }
 console.log(guessNumber(randoNum));
+
