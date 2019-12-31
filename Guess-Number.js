1aa778dd45ce1dd2c5ebc1caa619f623d6b6e0ac
Lockett Pundt <kalifornium@hotmail.com>
2019-12-30 19:31:59 -0500

    fixed it prompting you for a number after you entered one. i dont know why i had the prompt set to it's own variable

--- Guess-Number.js
+++ Guess-Number.js
@@ -3,12 +3,11 @@
 
 // for loop to give 10 guesses.
 function guessNumber(num) {
-	var guess = parseInt(prompt("Guess a number between 1 and 10. You have 10 guesses..."));
+	var x = parseInt(prompt("Guess a number between 1 and 10. You have 10 guesses..."));
 	var rando = Math.random();
 	var rando2 = (rando * 10) + 1;
 	var numField = Math.floor(rando2);
-    var x = num;
-    for (var i = 9; i > 0; i--) {
+    for (var i = 10; i > 0; i--) {
     	if (x >= 1 && x <= 10) {
         	if (x === numField) {
                   return alert("Congrats! You chose correctly!");
@@ -20,7 +19,8 @@ function guessNumber(num) {
         	}
         }
         else {
-          x = parseInt(prompt("Please chose a number between 1 and 10"));    
+		  x = parseInt(prompt("Please chose a number between 1 and 10"));
+		  x++;  
         }
     }
 }
